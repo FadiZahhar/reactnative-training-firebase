77a18b670229f5c02fbd5331683faa8a978e63bf
Fadi Zahhar <79freelancer@gmail.com>
2022-02-28 10:55:59 +0200

    adding the config of firebase

---
+++ config/config.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const config = {
+    apiKey: "AIzaSyDo3xbCPiibR3iipVejuNpm2jEMfjpJtCs",
+    authDomain: "fir-recipes-eaba0.firebaseapp.com",
+    databaseURL: "https://fir-recipes-eaba0-default-rtdb.firebaseio.com",
+    projectId: "fir-recipes-eaba0",
+    storageBucket: "fir-recipes-eaba0.appspot.com",
+    messagingSenderId: "994013080160",
+    appId: "1:994013080160:web:5ff807960fcc6c8509b105",
+    measurementId: "G-5EE408RC7M"
+  };
+
+  firebase.initializeApp(config);
