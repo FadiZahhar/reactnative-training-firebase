d8cb8a778f1521d4437be0a4637160eb34ff53eb
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2022-03-03 16:08:41 +0200

    Create Movies Data Service

--- App.js
+++ App.js
@@ -7,17 +7,23 @@
  */
 
 import React from 'react';
-import { Text, View } from 'react-native';
+import {Text, View} from 'react-native';
+import axios from 'axios';
+
+const getPopularMovies = async () => {
+  const resp = await axios.get(
+    'https://api.themoviedb.org/3/movie/popular?api_key=ef472b7ade9c300265343704a8187280&language=en-US',
+  );
+  console.log(JSON.stringify(resp.data.results[0], null, 2));
+};
 
 const App = () => {
-  
+  getPopularMovies();
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>
-        Try editing me! 🎉
-      </Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Try editing Ali! 🎉</Text>
     </View>
   );
-}
+};
 
 export default App;
