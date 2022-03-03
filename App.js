9e0b63ba8f9a0b6a0175e980f04249269af79076
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2022-03-03 16:21:46 +0200

    Display Movie Data

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
 import axios from 'axios';
 
@@ -14,14 +14,19 @@ const getPopularMovies = async () => {
   const resp = await axios.get(
     'https://api.themoviedb.org/3/movie/popular?api_key=ef472b7ade9c300265343704a8187280&language=en-US',
   );
-  console.log(JSON.stringify(resp.data.results[0], null, 2));
+  return resp.data.results;
 };
 
 const App = () => {
-  getPopularMovies();
+  const [movie, setMovie] = useState('');
+  getPopularMovies().then(movies => {
+    setMovie(movies[0]);
+  });
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Try editing Ali! 🎉</Text>
+      <Text>Title: {movie.original_title}</Text>
+      <Text>Language: {movie.original_language}</Text>
+      <Text>Release date: {movie.release_date}</Text>
     </View>
   );
 };
